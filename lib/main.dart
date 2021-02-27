de78964e64a37068a10b01bf9b5e40b046aa6135
Jonatas Henrique <jhenriquess26@gmail.com>
2021-02-27 17:31:36 -0300

    Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(DateChat());
 }
 
+class DateChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Date Chat',
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
